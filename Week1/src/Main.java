6f189ea9c1ea00ce579fbb0d82167c89cbfcb7ed
Twilight0908 <nguyenquang00.it@gmail.com>
2023-06-06 10:37:14 +0700

    Update File

--- Week1/src/Main.java
+++ Week1/src/Main.java
@@ -6,7 +6,12 @@ public class Main {
 //        System.out.println("Hello world!");
 //        SystemTime();
 //        RectArea();
-        LinearEquationResolver();
+//        LinearEquationResolver();
+//        DayOfMonth();
+//        LeapYear();
+//        Bmi();
+//        ChangeMoney();
+        NumberToString();
     }
 
     public static void SystemTime() {
@@ -50,5 +55,165 @@ public class Main {
                 System.out.println("no x");
             }
         }
+    }
+
+    public static void DayOfMonth() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Which month that you want to count days?");
+        int month = scanner.nextInt();
+        String dayIsMonth;
+
+        switch (month) {
+            case 2:
+                dayIsMonth = "28 or 29";
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                dayIsMonth = "31";
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                dayIsMonth = "30";
+                break;
+            default:
+                dayIsMonth = "";
+                break;
+        }
+
+        if (!dayIsMonth.equals(""))
+            System.out.printf("The month '%d' has %s days!", month, dayIsMonth);
+        else
+            System.out.println("Invalid input!");
+    }
+
+    public static void LeapYear() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a year");
+        int year = scanner.nextInt();
+        boolean leapYear = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    leapYear = true;
+                }
+            } else {
+                leapYear = true;
+            }
+        }
+
+        if (leapYear) {
+            System.out.printf("%d is a leap year", year);
+        } else {
+            System.out.printf("%d not a leap year", year);
+        }
+    }
+
+    public static void Bmi() {
+        Scanner scanner = new Scanner(System.in);
+        double weight, height, bmi;
+
+        System.out.println("Enter your weight (kg)");
+        weight = scanner.nextDouble();
+
+        System.out.println("Enter your height (m)");
+        height = scanner.nextDouble();
+
+        bmi = weight / Math.pow(height, 2);
+        if (bmi < 18) {
+            System.out.println(bmi + " Underweight");
+        } else if (bmi < 25) {
+            System.out.println(bmi + " Normal");
+        } else if (bmi < 30) {
+            System.out.println(bmi + " Overweight");
+        } else {
+            System.out.println(bmi + " Obese");
+        }
+    }
+
+    public static void ChangeMoney() {
+        Scanner scanner = new Scanner(System.in);
+        final double vnd = 23485;
+
+        System.out.println("Enter USD");
+        double usd = scanner.nextDouble();
+
+        System.out.println(usd * vnd + " VND");
+    }
+
+    public static void NumberToString() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number");
+        int number = scanner.nextInt();
+        String str = "";
+
+        int hundreds = number / 100;
+        int tens = (number / 10) % 10;
+        int ones = number % 10;
+
+        if (tens == 0 && ones == 0) {
+            str += getString(hundreds) + " Tram ";
+        } else {
+            if (hundreds != 0) {
+                str += getString(hundreds) + " Tram ";
+            }
+
+            if (tens == 0) {
+                str += " Linh ";
+            } else {
+                str += getString(tens) + " Muoi ";
+            }
+
+            if (ones != 0) {
+                str += getString(ones);
+            }
+        }
+
+        System.out.println(str);
+    }
+
+    public static String getString(int num) {
+        String str = "";
+        switch (num) {
+            case 1:
+                str = "Mot";
+                break;
+            case 2:
+                str = "Hai";
+                break;
+            case 3:
+                str = "Ba";
+                break;
+            case 4:
+                str = "Bon";
+                break;
+            case 5:
+                str = "Nam";
+                break;
+            case 6:
+                str = "Sau";
+                break;
+            case 7:
+                str = "Bay";
+                break;
+            case 8:
+                str = "Tam";
+                break;
+            case 9:
+                str = "Chin";
+                break;
+            default:
+                str = "";
+        }
+
+        return str;
     }
 }

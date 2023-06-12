65606605d8ae561e5ed5f91057d790b66116f7e0
Twilight0908 <nguyenquang00.it@gmail.com>
2023-06-12 14:38:52 +0700

    Update File

---
+++ Week2/src/Inheritance/geometry/Main.java
@@ -0,0 +1,45 @@
+package Inheritance.geometry;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Shape shape=new Shape();
+        System.out.println(shape);
+
+        shape=new Shape("red",false);
+        System.out.println(shape);
+         */
+
+        /*
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle(3);
+        System.out.println(circle);
+
+        circle = new Circle("black", true, 4);
+        System.out.println(circle);
+         */
+
+        /*
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+        System.out.println(rectangle.getArea());
+
+        rectangle = new Rectangle(2, 3);
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle("white", true, 4, 5);
+        System.out.println(rectangle);
+         */
+
+        Square square = new Square();
+        System.out.println(square);
+
+        square = new Square(2);
+        System.out.println(square);
+
+        square = new Square("yellow", true, 3);
+        System.out.println(square);
+    }
+}
---
+++ Week2/src/Inheritance/geometry/Square.java
@@ -0,0 +1,28 @@
+package Inheritance.geometry;
+
+public class Square extends Rectangle {
+    public Square() {
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(String color, boolean filled, double side) {
+        super(color, filled, side, side);
+    }
+
+    public double getside() {
+        return super.getWidth();
+    }
+
+    public void setSquare(double side) {
+        super.setWidth(side);
+        super.setHeight(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square{} " + super.toString();
+    }
+}

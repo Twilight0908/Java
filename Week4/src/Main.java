34266d3bac2e75f5ac4bd119562f18b0a485794e
Twilight0908 <nguyenquang00.it@gmail.com>
2023-06-29 15:58:39 +0700

    Update File

--- Week4/src/Main.java
+++ Week4/src/Main.java
@@ -1,5 +1,20 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("Swing Example");
+
+        frame.setSize(300, 200);
+
+        JLabel label = new JLabel("Hello World");
+        frame.add(label);
+
+        JButton button = new JButton("Button");
+        button.setSize(20,10);
+        frame.add(button);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
     }
 }
